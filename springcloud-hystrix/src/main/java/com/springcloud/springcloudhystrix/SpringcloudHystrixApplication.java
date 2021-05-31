f4a572f122bd492fe7f4549439ce4ff8a0b0d4b9
gaoyao <gaoyao@baicm.com.cn>
2021-05-31 16:20:08 +0800

    hystrix init

---
+++ springcloud-hystrix/src/main/java/com/springcloud/springcloudhystrix/SpringcloudHystrixApplication.java
@@ -0,0 +1,20 @@
+package com.springcloud.springcloudhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
+public class SpringcloudHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudHystrixApplication.class, args);
+    }
+
+}
+
---
+++ springcloud-hystrix/src/test/java/com/springcloud/springcloudhystrix/SpringcloudHystrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.springcloudhystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudHystrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
