2cf65b95ed40683f76d11512e652b2acca78aad5
gaoyao <gaoyao@baicm.com.cn>
2021-07-26 08:37:24 +0800

    分配

---
+++ springcloud-jbs-demo/src/main/java/com/springcloud/jbsdemo/JbsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.jbsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbsDemoApplication.class, args);
+	}
+
+}
---
+++ springcloud-jbs-demo/src/test/java/com/springcloud/jbsdemo/JbsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.jbsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
